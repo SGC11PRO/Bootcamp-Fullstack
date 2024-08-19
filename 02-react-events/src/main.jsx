35fc924eaf17780b9a8e7dd0c7823a53b4541a2d
Sergio G <sergio.garcia.cristobal@gmail.com>
2024-08-19 21:43:58 +0200

    React Events 1
    Testing w/ react events. Add useState hook to counter element

--- 02-react-events/src/main.jsx
+++ 02-react-events/src/main.jsx
@@ -1,6 +1,28 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import ReactDOM from 'react-dom' // importante para poder utilizar el ReactDOM.render
+import { useState } from 'react' // primer hook
 
-createRoot(document.getElementById('root')).render(
-  <App />
+// extrae 'root' a una variable separada
+const rootElement = document.getElementById('root');
+
+
+// componente App
+const App = (props) => {
+
+  let [contador_value, contador_update] = useState(0)
+
+  setInterval (() => {
+    contador_update(contador_value + 1) // contador_update (nuevo_valor)
+  }, 2000) // update cada 2000 ms
+
+  console.log(contador_value)
+  
+  return (
+    <h1>{contador_value}</h1>
+  )
+}
+
+// root render
+ReactDOM.render (
+  <App />,
+  rootElement
 )
