e00b0b28550e385f5d320b00d3cc128cc5a92bc6
Sergio G <sergio.garcia.cristobal@gmail.com>
2024-08-20 08:50:56 +0200

    Button onClick event
    Incrementar contador con boton y evento onClick()

--- 02-react-events/src/main.jsx
+++ 02-react-events/src/main.jsx
@@ -8,16 +8,31 @@ const rootElement = document.getElementById('root');
 // componente App
 const App = (props) => {
 
-  let [contador_value, contador_update] = useState(0)
+  const [contador_value, contador_update] = useState(0)
 
+  /*
+  const contador = useState(0)
+  const contador_value = useState[0]
+  const contador_update = useState[1]
+  */
+
+  /*
   setInterval (() => {
     contador_update(contador_value + 1) // contador_update (nuevo_valor)
   }, 2000) // update cada 2000 ms
+  */
 
-  console.log(contador_value)
+
+  const ButtonFunction = () => {
+    contador_update(contador_value + 1)
+  }
   
   return (
+    <>
     <h1>{contador_value}</h1>
+    <button onClick={ButtonFunction}>
+      Incrementar Contador
+    </button></>
   )
 }
 
