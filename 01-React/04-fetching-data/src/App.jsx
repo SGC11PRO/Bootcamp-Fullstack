3467f80cbd67fa17d5ee6d7bc4f24cd0aaa5216b
Sergio G <sergio.garcia.cristobal@gmail.com>
2024-08-22 17:05:25 +0200

    axios & POST a la API

--- 01-React/04-fetching-data/src/App.jsx
+++ 01-React/04-fetching-data/src/App.jsx
@@ -1,5 +1,7 @@
 import { useState, useEffect, useDeferredValue } from 'react'
 import React from 'react'
+import axios from 'axios' // similar a metodo fetch
+
 import { Note } from './Note'
 
 export default function App(props) {
@@ -10,14 +12,29 @@ export default function App(props) {
   // el use effect se ejecuta cuando se actualiza un estado (o las dependencias que tu especifiques) -> las ponemos en los [] al final.
   // si los [] estan vacios, solo se ejecutara una vez, cuando se renderice por primera vez la pagina
   useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/posts') // IMPORTANTE LOS FETCH SOLO LLAMARLOS 1 VEZ PARA EVITAR BUCLES INFINITOS
-      .then(response => response.json())
-        .then((json) => {
-          console.log('fetch realizado')
+
+    console.log('use effect ha sido llamado')
+
+    setTimeout(() => {
+      /*
+      fetch('https://jsonplaceholder.typicode.com/posts') // IMPORTANTE LOS FETCH SOLO LLAMARLOS 1 VEZ PARA EVITAR BUCLES INFINITOS
+      .then(response => response.json()) // transforma la respuesta en un array json
+        .then((json) => { // pasa el array a notes
+
           setNotes(json)
         })
+        
+      */
 
-  }, [])
+        axios
+          .get('https://jsonplaceholder.typicode.com/posts')
+          .then((response) => {
+            const {data} = response // unicamente nos interesa el atributo 'data' que es donde se encuentran los obj de la API
+            setNotes(data)
+          })
+
+    }, 200) // espera X sec para realizar el fetch
+  }, []) // cuando newNote cambie, se llamara de nuevo al useEffect
 
   const handleChange = (event) => {
     setNewNote(event.target.value)
@@ -26,15 +43,21 @@ export default function App(props) {
   const handleSubmit = (event) => {
     event.preventDefault()
 
-    console.log('crear nota')
-
     const noteToAddState = {
-      id: notes.lenght + 1,
       title: newNote,
-      body: newNote
+      body: newNote,
+      userId: 1
     }
 
-    setNewNote((prevNotes) => prevNotes.concat(noteToAddState))
+    // modificar API y guardar nueva nota en el servidor -> .POST
+    axios
+      .post('https://jsonplaceholder.typicode.com/posts', noteToAddState)
+      .then(response => {
+        const {data} = response
+        setNotes((prevNotes) => prevNotes.concat(data))
+
+      })
+
     setNewNote('')
   }
 
@@ -52,7 +75,7 @@ export default function App(props) {
       </ol>
 
       <form onSubmit={handleSubmit}>
-        <input type="text" onChange={handleChange}/>
+        <input type="text" onChange={handleChange} value={newNote}/>
         <button>Crear Nota</button>
       </form>
     </div>
